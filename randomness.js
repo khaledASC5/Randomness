f8cbbdfd16a2dc66e527b50d83ffce4b78a68c53
khaledASCS <khaled.muthana08@gmail.com>
2018-07-12 10:07:26 -0500

    I commit I will try

---
+++ randomness.js
@@ -0,0 +1,32 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+// const alphabet = "abcdefghijklmnopqrstuvwxyz";
+// const foods = [
+//     "pizza",
+//     "burgers",
+//     "rice and beans",
+//     "lasagna",
+//     "macaroni and cheese",
+//     "baked potatoes"
+// ];
+// const students = [
+//     "Bob",
+//     "Martin",
+//     "Danny",
+//     "Mahdi",
+//     "Harry",
+//     "Sean",
+//     "Robert"
+// ];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
+function randomNumber(){
+    var randNum = Math.floor(Math.random() * (10)) + 1;
+    console.log(randNum);
+randomNumber()
+} 
+function randomLetter(){
+
+}
